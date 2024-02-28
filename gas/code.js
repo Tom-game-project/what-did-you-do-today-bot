5c7ba69dd60efccfc057b75db4a52200221d4076
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-02-28 15:00:03 +0900

    add embed

--- gas/code.js
+++ gas/code.js
@@ -2,10 +2,31 @@ const WEBHOOK_URL = PropertiesService.getScriptProperties().getProperty("WEBHOOK
 
 
 function myFunction() {
-   let payload =
+  const colorCode = parseInt("ff6fac", 16);
+  let today = new Date();
+  let date_string = Utilities.formatDate(today, 'JST', 'yyyy-MM-dd');
+  let payload =
   {
     "avatar_url":"https://lh3.googleusercontent.com/d/1nOOSH_vTrLR8_kJl1TM_lc_3CZpGZCd4",
-    "content": "今日何した？"
+    "content": "今日何した？",
+    "embeds": [
+      {
+        // 本文
+        "title": "今日何した？", 
+        "color": colorCode,
+        "fields": [
+          {
+            "name": ":date:今日の日付",
+            "value": date_string,
+            "inline": true
+          },
+        ],
+        "footer": {
+          "text": "https://github.com/Tom-game-project/what-did-you-do-today-bot",
+          "icon_url": "https://lh3.googleusercontent.com/d/1B4NImJgaULqngOJClCN2NIPYK21M9mdS"
+        },
+      }
+    ]
   };
   const replyHeaders = {
     'Content-Type': 'application/json'
